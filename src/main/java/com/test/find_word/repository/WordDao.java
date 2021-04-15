ce6a38287ef4b49c53a7a24b392351f12f8bc151
pwang <rewind1224@gmail.com>
2021-04-16 02:11:20 +0900

    .repository.WordDao.java fixed error to 'autowire'(serviceImpl.java) @Repository

--- src/main/java/com/test/find_word/repository/WordDao.java
+++ src/main/java/com/test/find_word/repository/WordDao.java
@@ -1,9 +1,11 @@
 package com.test.find_word.repository;
 
 import com.test.find_word.model.WordModel;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface WordDao {
 
     List<WordModel> getWord();
